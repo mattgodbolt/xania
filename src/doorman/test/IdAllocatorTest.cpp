6dec2c53843d82341ac0e9d7ce582334cdb8d870
Matt Godbolt <matt@godbolt.org>
2020-08-01 10:41:29 -0500

    Part three of doorman PR (see #54)
    Builds on #81
    
    A general purpose ID allocator. The allocator gives out small integer identifiers, along with a reference-counted reservation that holds them as "in use".
    
    IDs are eagerly recycled once the reference count hits zero.
    
    Useful for Xania and doorman to both have a "vote" for which channels are in-use. Previously we had bugs where there was a race in doorman if someone disconnected while xania was about to send a ton of info while another connection came in. The new connection would be given the channel ID of the recently-closed connection and would thus be delivered all the messages for the old connection.

---
+++ src/doorman/test/IdAllocatorTest.cpp
@@ -0,0 +1,50 @@
+#include "IdAllocator.hpp"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Id allocator", "[IdAllocator]") {
+    IdAllocator ia;
+    SECTION("should hand out zero first") { CHECK(ia.reserve()->id() == 0); }
+    SECTION("should hand out sequential numbers") {
+        std::vector<IdAllocator::Reservation> reservations;
+        for (auto i = 0u; i < 100u; ++i) {
+            reservations.emplace_back(ia.reserve());
+            REQUIRE(reservations.back()->id() == i);
+        }
+        SECTION("and releases") {
+            reservations.clear(); // it's not specified which order the STL
+            CHECK(ia.reserve()->id() < 100u);
+        }
+        SECTION("handing back the most recently used first") {
+            for (auto &r : reservations)
+                r.reset();
+            CHECK(ia.reserve()->id() == 99u);
+        }
+    }
+    SECTION("should keep reservations active out of order") {
+        auto res0 = ia.reserve();
+        auto res1 = ia.reserve();
+        auto res2 = ia.reserve();
+        SECTION("recycles the middle id") {
+            res1.reset();
+            CHECK(ia.reserve()->id() == 1);
+        }
+        SECTION("recycles the first id") {
+            res0.reset();
+            CHECK(ia.reserve()->id() == 0);
+        }
+        SECTION("recycles the last id") {
+            res2.reset();
+            CHECK(ia.reserve()->id() == 2);
+        }
+    }
+    SECTION("should reference count references") {
+        auto res = ia.reserve();
+        auto res_copy = res;
+        CHECK(ia.reserve()->id() == 1);
+        res.reset();
+        CHECK(ia.reserve()->id() == 1);
+        res_copy.reset();
+        CHECK(ia.reserve()->id() == 0);
+    }
+}
