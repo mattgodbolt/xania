2104504597e96b3945ca5493d33b330fcba8d991
snellers <1669156+snellers@users.noreply.github.com>
2020-12-17 21:20:06 +0000

    mobs-adjust-armour-class.pl script to improve (lower) the AC of all mobs so the corresponding runtime hack can be removed (#250). Also remove gimp.are as we won't use it.

--- scripts/rooms-remove-areanum.pl
+++ scripts/rooms-remove-areanum.pl
@@ -4,25 +4,25 @@
 # This field was never used. We can probably adapt this script to do other
 # tactical area file massagery in future.
 
-my $in_rooms = false;
-my $in_room = false;
+my $in_rooms = 0;
+my $in_room = 0;
 my $room_tildes = 0;
 
 while (<>) {
     if (/#ROOMS/) {
-        $in_rooms = true;
+        $in_rooms = 1;
     } elsif ((/#[A-Z]{2,}/)) {  # OBJECTS, MOBILES, RESETS, HELPS etc.
-        $in_rooms = false;
+        $in_rooms = 0;
     }
     if ($in_rooms) {
         if (/#[\d]+/) { # room vnum
-            $in_room = true;
+            $in_room = 1;
             $room_tildes = 0;
         } elsif ($in_room && /\~/) {
             $room_tildes++;
         } elsif ($room_tildes == 2 && /^([\d]+)\s+(\S+)\s+(\S+)\s*$/) {
             print "$2 $3\n";
-            $in_room = false;
+            $in_room = 0;
             next;
         }
     }
